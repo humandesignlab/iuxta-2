b5f9ebd61179089690269a8459790fe4ef69694e
Humberto Mangino <hmangino@gmail.com>
2018-10-16 11:39:01 -0600

    adds gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a24ebff2c006faf20d1";
+/******/ 	var hotCurrentHash = "7416fc6d9955209f4361";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
