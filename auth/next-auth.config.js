73c96ce718328f07b1704fc70f6e1d765b907a6d
Humberto Mangino <hmangino@gmail.com>
2019-01-16 11:30:06 -0700

    add next.config.js file

--- auth/next-auth.config.js
+++ auth/next-auth.config.js
@@ -46,7 +46,7 @@ module.exports = () => {
 				// Define a port (if none passed, will not start Express)
 				port: process.env.PORT || 3000,
 				// Secret used to encrypt session data on the server.
-				sessionSecret: "change-me",
+				sessionSecret: "691F2F2A6B87349A28FC96229ED98",
 				// Maximum Session Age in ms (optional, default is 7 days).
 				// The expiry time for a session is reset every time a user revisits
 				// the site or revalidates their session token. This is the maximum
