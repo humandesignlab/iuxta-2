6bfb2b493610e3f81b4b547efb0fb265ac5c86ea
Humberto Mangino <hmangino@gmail.com>
2019-01-16 15:49:51 -0700

    add server url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").load();
 // Initialize Next.js
 const nextApp = next({
   dir: ".",
-  dev: process.env.NODE_ENV === "development"
+  dev: process.env.NODE_ENV === "production"
 });
 
 // Add next-auth to next app
@@ -24,7 +24,7 @@ nextApp
     return nextAuth(nextApp, nextAuthOptions);
   })
   .then(response => {
-    console.log(`Ready on ${process.env.SERVER_URL}:${process.env.PORT || 3000}`);
+    console.log(`Ready on http://localhost:${process.env.PORT || 3000}`);
   })
   .catch(err => {
     console.log("An error occurred, unable to start the server");
