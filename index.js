4a3bae4c7cdec750894e413072692ce395cc5989
Humberto Mangino <hmangino@gmail.com>
2019-01-17 18:23:10 -0700

    env production

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").load();
 // Initialize Next.js
 const nextApp = next({
   dir: ".",
-  dev: process.env.NODE_ENV === "development"
+  dev: process.env.NODE_ENV === "production"
 });
 
 // Add next-auth to next app
